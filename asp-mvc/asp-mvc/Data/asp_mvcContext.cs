394179dd09d6eb26871b3fc9f75befe623c62e38
mazoorek <piotr.mazurek98@gmail.com>
2020-05-13 19:17:19 +0200

    done microsoft .net tutorial task

---
+++ asp-mvc/asp-mvc/Data/asp_mvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace asp_mvc.Data
+{
+    public class asp_mvcContext : DbContext
+    {
+        public asp_mvcContext (DbContextOptions<asp_mvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; }
+    }
+}
